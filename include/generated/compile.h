50c6db99334fddde75c6aa21b370650cb63cdb9d
Viraj Kanwade <github@virajk.otherinbox.com>
2014-12-04 22:20:23 -0800

    Initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 26 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#26 SMP PREEMPT Tue Dec 2 11:44:20 PST 2014"
+#define LINUX_COMPILE_BY "virajk"
+#define LINUX_COMPILE_HOST "android"
+#define LINUX_COMPILER "gcc version 4.8 (GCC) "
